76c6bce5835ad166dd7e803186eaa2300d4aa494
Lawrence Ikhidero <43246123+lorens247@users.noreply.github.com>
2023-06-07 08:42:33 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,17 +10,17 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     // Allocate memory for the new node
-    binary_tree_t *node;
-    node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (node == NULL)
+    binary_tree_t *Node;
+    Node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+    if (Node == NULL)
         return (NULL);
 
     // Set the parent, value, left child, and right child of the new node
-    node->parent = parent;
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
+    Node->parent = parent;
+    Node->n = value;
+    Node->left = NULL;
+    Node->right = NULL;
 
     // Return a pointer to the new node
-    return (node);
+    return (Node);
 }
