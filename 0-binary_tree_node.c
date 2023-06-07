21b3723d754f58df27f8d5d6a1ace877fd490b6a
Lawrence Ikhidero <43246123+lorens247@users.noreply.github.com>
2023-06-07 08:56:16 +0100

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     /* Allocate memory for the new node */
     binary_tree_t *node;
+    
     node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
     if (node == NULL)
         return (NULL);
